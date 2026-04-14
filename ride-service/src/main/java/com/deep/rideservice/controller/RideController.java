93ea390990ec65a7c4898d4724db455752395066
Deep Saha <dip23447@gmail.com>
2026-04-14 22:25:53 +0530

    Ride search Endpoint for rider.

--- ride-service/src/main/java/com/deep/rideservice/controller/RideController.java
+++ ride-service/src/main/java/com/deep/rideservice/controller/RideController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/rides")
 @Slf4j
@@ -39,6 +41,10 @@ public class RideController {
     }
 
     // Get rides for rider
-
+    public ResponseEntity<List<RideResponse>> getRidesByRiderId(
+            @PathVariable String riderId
+    ){
+        return ResponseEntity.ok(rideService.getRidesByRiderId(riderId));
+    }
 
 }
