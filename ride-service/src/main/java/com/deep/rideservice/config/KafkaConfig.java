4361e5e0b9d91b2d0321d9f08819ee5e33264339
Deep Saha <dip23447@gmail.com>
2026-04-18 20:31:29 +0530

    Writing kafka configuration.

--- ride-service/src/main/java/com/deep/rideservice/config/KafkaConfig.java
+++ ride-service/src/main/java/com/deep/rideservice/config/KafkaConfig.java
@@ -1,15 +1,25 @@
 package com.deep.rideservice.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfig {
 
 //    Topic where ride service publishes ride request
 //    Matching services subscribe to this topic
+
+    @Bean
     public NewTopic rideRequestedTopic(){
-        
+        return TopicBuilder.name("ride.requetted")
+                .partitions(3)
+                .replicas(1)
+                .build();
     }
 
+
+    // Topic where matching service published match results
+    // Ride service subscribe to this topic
 }
