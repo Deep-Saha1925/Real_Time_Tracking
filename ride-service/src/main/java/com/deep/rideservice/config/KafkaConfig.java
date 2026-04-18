a0a0f24dfa57deaedf28cb692a3ca7977ca5f888
Deep Saha <dip23447@gmail.com>
2026-04-18 20:25:39 +0530

    Writing kafka configuration.

--- ride-service/src/main/java/com/deep/rideservice/config/KafkaConfig.java
+++ ride-service/src/main/java/com/deep/rideservice/config/KafkaConfig.java
@@ -1,4 +1,15 @@
 package com.deep.rideservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class KafkaConfig {
+
+//    Topic where ride service publishes ride request
+//    Matching services subscribe to this topic
+    public NewTopic rideRequestedTopic(){
+        
+    }
+
 }
