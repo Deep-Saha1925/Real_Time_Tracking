5029b25e0e4980ed805a28b3ad7dcdb6e984a397
Deep Saha <dip23447@gmail.com>
2026-04-18 21:38:13 +0530

    Added Feign client dependency and dtos for data exchange.

--- matching-service/src/main/java/com/deep/matchingservice/event/RideMatchedEvent.java
+++ matching-service/src/main/java/com/deep/matchingservice/event/RideMatchedEvent.java
@@ -1,4 +1,24 @@
 package com.deep.matchingservice.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Event published to Kafka topic: ride.matched
+ * Consumed by ride service to update ride to assigned driver
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RideMatchedEvent {
+
+    private String rideId;
+    private String riderId;
+    private String driverId;
+    private double driverLatitude;
+    private double driverLongitude;
+    private double distanceToPickupKm;
+
 }
--- matching-service/src/main/java/com/deep/matchingservice/event/RideRequestedEvent.java
+++ matching-service/src/main/java/com/deep/matchingservice/event/RideRequestedEvent.java
@@ -1,4 +1,28 @@
 package com.deep.matchingservice.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Event consumed from KAFKA topic: ride.requested
+ * Published by Ride service when a rider request a ride
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RideRequestedEvent {
+
+    private String rideId;
+    private String riderId;
+
+    private double pickupLatitude;
+    private double pickupLongitude;
+    private String pickupAddress;
+
+    private double dropLatitude;
+    private double dropLongitude;
+    private String dropAddress;
+
 }
